c49e32bc239ff2d80460efabb0b8ae8072937f52
Jonier Cardona Martinez <Joniercar96@outlook.com>
2025-03-07 14:03:42 -0500

    formulario simulador
    se realizar el formulario simulador

--- src/components/simluador-creditos/formulario/Inputs.tsx
+++ src/components/simluador-creditos/formulario/Inputs.tsx
@@ -4,119 +4,102 @@ import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
 import Input from "../input/InputField";
 import Select from "../Select";
+import Button from "../../ui/button/Button";
+import { useModal } from "../../../hooks/useModal";
+import Table from "@/components/tables/BasicTableOne";
 import { CalenderIcon, ChevronDownIcon, EyeCloseIcon, EyeIcon, TimeIcon } from "../../../icons";
 
 export default function DefaultInputs() {
   // Data
   const [showPassword, setShowPassword] = useState(false);
   const options = [
-    { value: "marketing", label: "Marketing" },
-    { value: "template", label: "Template" },
-    { value: "development", label: "Development" },
+    { value: "Mensual", label: "Mensual" },
+    { value: "Trimestar", label: "Trimestal" },
+    { value: "Semestral", label: "Semestal" },
+    { value: "Único, al vencmiento", label: "Único, al vencmiento" },
   ];
+  const { isOpen, openModal, closeModal } = useModal();
+    const handleSave = () => {
+      // Handle save logic here
+      console.log("Saving changes...");
+      closeModal();
+    };
+
+  const options2 = [
+    { value: "0", label: "0%" },
+    { value: "1.8", label: "1.8%" }
+   ]
 
   const handleSelectChange = (value: string) => {
     console.log("Selected value:", value);
   };
   return (
-    <ComponentCard title="Simluador de créditos" className="font-medium">
+    <><ComponentCard title="Simluador de créditos" className="font-medium">
       <div className="space-y-6">
         <div>
-          <Label>Cuenta</Label>
-          <Input type="number" />
-        </div>
-        <div>
-          <Label>Input with Placeholder</Label>
-          <Input type="text" placeholder="info@gmail.com" />
-        </div>
-        <div>
-          <Label>Select Input</Label>
-          <div className="relative">
-            <Select
-            options={options}
-            placeholder="Select an option"
-            onChange={handleSelectChange}
-            className="dark:bg-dark-900"
-          />
-             <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <ChevronDownIcon/>
-            </span>
-          </div>
-        </div>
-        <div>
-          <Label>Password Input</Label>
-          <div className="relative">
-            <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter your password"
-            />
-            <button
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute z-30 -translate-y-1/2 cursor-pointer right-4 top-1/2"
-            >
-              {showPassword ? (
-                <EyeIcon className="fill-gray-500 dark:fill-gray-400" />
-              ) : (
-                <EyeCloseIcon className="fill-gray-500 dark:fill-gray-400" />
-              )}
-            </button>
-          </div>
-        </div>
-        <div>
-          <Label htmlFor="datePicker">Date Picker Input</Label>
+          <Label htmlFor="datePicker">Fecha de disposición</Label>
           <div className="relative">
             <Input
               type="date"
               id="datePicker"
               name="datePicker"
-              onChange={(e) => console.log(e.target.value)}
-            />
+              onChange={(e) => console.log(e.target.value)} />
             <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
               <CalenderIcon />
             </span>
           </div>
         </div>
+        {/* <div>
+      <Label>Cuenta</Label>
+      <Input type="number" />
+    </div> */}
         <div>
-          <Label htmlFor="tm">Date Picker Input</Label>
+          <Label>Linea del Credito</Label>
+          <Input type="text" />
+        </div>
+        <div>
+          <Label>Monto del Credito</Label>
+          <Input type="number" />
+        </div>
+        <div>
+          <Label>Forma de Pago</Label>
           <div className="relative">
-            <Input
-              type="time"
-              id="tm"
-              name="tm"
-              onChange={(e) => console.log(e.target.value)}
-            />
+            <Select
+              options={options}
+              placeholder="Selecione una opción"
+              onChange={handleSelectChange}
+              className="dark:bg-dark-900" />
             <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <TimeIcon />
+              <ChevronDownIcon />
             </span>
           </div>
         </div>
+
         <div>
-          <Label htmlFor="tm">Input with Payment</Label>
+          <Label>Taza de Interes</Label>
           <div className="relative">
-            <Input
-              type="text"
-              placeholder="Card number"
-              className="pl-[62px]"
-            />
-            <span className="absolute left-0 top-1/2 flex h-11 w-[46px] -translate-y-1/2 items-center justify-center border-r border-gray-200 dark:border-gray-800">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="6.25" cy="10" r="5.625" fill="#E80B26" />
-                <circle cx="13.75" cy="10" r="5.625" fill="#F59D31" />
-                <path
-                  d="M10 14.1924C11.1508 13.1625 11.875 11.6657 11.875 9.99979C11.875 8.33383 11.1508 6.8371 10 5.80713C8.84918 6.8371 8.125 8.33383 8.125 9.99979C8.125 11.6657 8.84918 13.1625 10 14.1924Z"
-                  fill="#FC6020"
-                />
-              </svg>
+            <Select
+              options={options2}
+              placeholder="Selecione una opción"
+              onChange={handleSelectChange}
+              className="dark:bg-dark-900" />
+            <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+              <ChevronDownIcon />
             </span>
           </div>
+        </div>
+        <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
+          <Button size="sm" variant="outline" onClick={closeModal}>
+            Cancelar
+          </Button>
+          <Button size="sm" onClick={handleSave}>
+            Simular
+          </Button>
         </div>
       </div>
     </ComponentCard>
+    <ComponentCard title="Simluador de créditos" className="font-medium">
+      <Table />
+    </ComponentCard></>
   );
 }
